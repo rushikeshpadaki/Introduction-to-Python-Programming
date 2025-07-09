0f918c64bd1ec80c0253306db62a3b3ab335b72d
rushikeshpadaki <32383706+rushikeshpadaki@users.noreply.github.com>
2025-07-09 10:44:52 +0530

    Add files via upload

---
+++ 03_Practice_Programs/02_AddTwoNums.py
@@ -0,0 +1,36 @@
+"""
+Title: Addition of Two Floating Point Numbers
+Author: Rushikesh Padaki
+Date: 09 July 2025
+
+Description:
+A Python program to add two floating-point numbers and display the result.
+- Demonstrates the use of variables to store floating-point numbers.
+- Performs addition using the '+' operator.
+- Displays the result using the print() function.
+
+Algorithm:
+1. Initialize the first floating-point number and store it in variable num1.
+2. Initialize the second floating-point number and store it in variable num2.
+3. Add num1 and num2 using the '+' operator and store the result in the variable total.
+4. Print the value of total using the print() function.
+
+Time Complexity:
+- O(1): The program performs a constant-time addition operation.
+
+Space Complexity:
+- O(1): The program uses a constant amount of space to store three variables.
+
+Sample Execution:
+
+Case 1: Basic execution
+Output:
+7.8
+"""
+
+num1 = 1.5
+num2 = 6.3
+
+total = num1 + num2
+
+print(total)
