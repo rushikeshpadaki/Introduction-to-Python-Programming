6ccff1e5b54e2d4885de62eebfa198f79b02608d
rushikeshpadaki <32383706+rushikeshpadaki@users.noreply.github.com>
2025-07-11 23:03:05 +0530

    Add files via upload

---
+++ 03_Practice_Programs/10_SetOperations.py
@@ -0,0 +1,7 @@
+A = {2, 4, 6, 8, 10}
+B = {1, 2, 3, 4, 5}
+
+print('Union of Sets A and B: ', A | B)
+print('Intersection of Sets A and B: ', A & B)
+print('Difference of Sets A and B: ', A - B)
+print('Symmetric Difference of Sets A and B: ', A ^ B)
